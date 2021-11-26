e0e848bf428616ee19dcebbbe3b2890eb42d842f
kymmeh <kymmehayley@gmail.com>
2021-11-26 13:45:45 +0000

    Review tweaks

--- src/views/sole-to-joint-view/sole-to-joint-view.test.tsx
+++ src/views/sole-to-joint-view/sole-to-joint-view.test.tsx
@@ -6,16 +6,16 @@ import { SoleToJointView } from "./sole-to-joint-view";
 
 test("it renders sole-to-joint initiate view", () => {
   render(<SoleToJointView />, {
-    url: "/processes/:entityType/:id/sole-to-joint",
-    path: "/processes/:entityType/:id/sole-to-joint",
+    url: "/processes/sole-to-joint",
+    path: "/processes/sole-to-joint",
   });
   expect(screen.getByText(locale.processes.soleToJoint.title)).toBeInTheDocument();
 });
 
 test("it renders sole-to-joint view for a started process", () => {
   render(<SoleToJointView />, {
-    url: "/processes/:entityType/:id/sole-to-joint/e63e68c7-84b0-3a48-b450-896e2c3d7735",
-    path: "/processes/:entityType/:id/sole-to-joint/:processId",
+    url: "/processes/sole-to-joint/e63e68c7-84b0-3a48-b450-896e2c3d7735",
+    path: "/processes/sole-to-joint/:processId",
   });
   expect(screen.queryByText(locale.processes.soleToJoint.title)).not.toBeInTheDocument();
 });
--- src/views/sole-to-joint-view/sole-to-joint-view.tsx
+++ src/views/sole-to-joint-view/sole-to-joint-view.tsx
@@ -1,15 +1,11 @@
-import { useRouteMatch } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { SoleToJointInitiate } from "../../components/sole-to-joint";
 
-export interface MatchParams {
-  processId: string;
-}
-
 export const SoleToJointView = () => {
-  const { params } = useRouteMatch<MatchParams>();
+  const { processId } = useParams<{ processId: string }>();
 
-  if (!params.processId) {
+  if (!processId) {
     return <SoleToJointInitiate />;
   }
 
