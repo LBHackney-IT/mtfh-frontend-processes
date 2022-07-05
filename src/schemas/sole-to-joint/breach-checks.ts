2b9089158b8a25bafb4a3668333ae8b4c7f8a425
dzulix <julia.wojciechowska@hackney.gov.uk>
2022-07-05 08:23:31 +0200

    MTTL-2867: Update schemas.

--- src/schemas/sole-to-joint/breach-checks.ts
+++ src/schemas/sole-to-joint/breach-checks.ts
@@ -2,22 +2,10 @@ import * as Yup from "yup";
 
 export const breachChecksFormSchema = (errorMessages: Record<string, string>) =>
   Yup.object({
-    br5: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br10: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br17: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br18: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
+    br5: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br10: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br17: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br18: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
   });
 
 export type BreachChecksFormData = Yup.Asserts<ReturnType<typeof breachChecksFormSchema>>;
--- src/schemas/sole-to-joint/further-eligibility.ts
+++ src/schemas/sole-to-joint/further-eligibility.ts
@@ -2,34 +2,13 @@ import * as Yup from "yup";
 
 export const furtherEligibilityFormSchema = (errorMessages: Record<string, string>) =>
   Yup.object({
-    br11: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br12: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br13: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br15: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br16: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br7: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
-    br8: Yup.string()
-      .oneOf(["true", "false", null])
-      .required(errorMessages.W4)
-      .nullable(true),
+    br11: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br12: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br13: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br15: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br16: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br7: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
+    br8: Yup.string().oneOf(["true", "false"]).required(errorMessages.W4),
   });
 
 export type FurtherEligibilityFormData = Yup.Asserts<
