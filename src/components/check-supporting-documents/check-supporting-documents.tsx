cd32f7ad90af09e594a4096b09313fde6febb5d1
dzulix <julia.wojciechowska@hackney.gov.uk>
2022-08-02 13:53:02 +0200

    MTTL-3124: Add Tenant Declaration to Request Documents for Change of Name process.

--- src/components/check-supporting-documents/check-supporting-documents.tsx
+++ src/components/check-supporting-documents/check-supporting-documents.tsx
@@ -28,7 +28,7 @@ export const CheckSupportingDocuments = ({
   mutate,
   optional,
 }: ProcessComponentProps): JSX.Element => {
-  const { person, tenant, declaration } = optional;
+  const { person, tenant } = optional;
   const errorMessages = useErrorCodes();
 
   if (!errorMessages) {
@@ -67,7 +67,6 @@ export const CheckSupportingDocuments = ({
           hour: "",
           minute: "",
           amPm: "",
-          isDeclaration: declaration,
           declaration: false,
         }}
         validateOnBlur={false}
@@ -161,39 +160,35 @@ export const CheckSupportingDocuments = ({
 
               {requestType === "manual" && <DateTimeFields errors={errors} />}
 
-              {declaration && (
-                <>
-                  <Heading variant="h3">Tenant declaration</Heading>
-                  <Text size="sm">
-                    Please read this out to the applicant before you proceed to the next
-                    step. Applicant has to accept the declaration before the application
-                    can be proceeded:
-                  </Text>
-                  <Text size="sm">
-                    “You are declaring that to the best of your knowledge and belief the
-                    information given is correct in every detail. You understand it is an
-                    offence to give false or misleading information or to hold back
-                    relevant information. You also understand that we will check this
-                    information and if any information is found to be false, you may be
-                    prosecuted and we may repossess your home.
-                  </Text>
-                  <Text size="sm">
-                    If you are prosecuted and found guilty, you understand that you could
-                    be ordered to pay a fine of up to £5000. It is your duty to make sure
-                    that your application form is honest. You hereby give your consent for
-                    Housing Services to carry out a credit reference check and obtain
-                    relevant information from the Council and other external agencies.”
-                  </Text>
+              <Heading variant="h3">Tenant declaration</Heading>
+              <Text size="sm">
+                Please read this out to the applicant before you proceed to the next step.
+                Applicant has to accept the declaration before the application can be
+                proceeded:
+              </Text>
+              <Text size="sm">
+                “You are declaring that to the best of your knowledge and belief the
+                information given is correct in every detail. You understand it is an
+                offence to give false or misleading information or to hold back relevant
+                information. You also understand that we will check this information and
+                if any information is found to be false, you may be prosecuted and we may
+                repossess your home.
+              </Text>
+              <Text size="sm">
+                If you are prosecuted and found guilty, you understand that you could be
+                ordered to pay a fine of up to £5000. It is your duty to make sure that
+                your application form is honest. You hereby give your consent for Housing
+                Services to carry out a credit reference check and obtain relevant
+                information from the Council and other external agencies.”
+              </Text>
 
-                  <FormGroup id="declaration-form-group" error={errors.declaration}>
-                    <InlineField name="declaration" type="checkbox">
-                      <Checkbox id="declaration" data-testid="declaration">
-                        I confirm that the applicant has accepted this declaration
-                      </Checkbox>
-                    </InlineField>
-                  </FormGroup>
-                </>
-              )}
+              <FormGroup id="declaration-form-group" error={errors.declaration}>
+                <InlineField name="declaration" type="checkbox">
+                  <Checkbox id="declaration" data-testid="declaration">
+                    I confirm that the applicant has accepted this declaration
+                  </Checkbox>
+                </InlineField>
+              </FormGroup>
               <Button type="submit" disabled={!dirty}>
                 Next
               </Button>
