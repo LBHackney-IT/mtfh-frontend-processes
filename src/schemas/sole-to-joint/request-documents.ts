eb6b3a21689bb205ffe49be6a8b8ba8a318eb203
dzulix <dzulix@outlook.com>
2022-08-02 14:34:04 +0200

    MTTL-3124: Add Tenant Declaration to Request Documents for Change of Name process. (#131)
    Co-authored-by: dzulix <julia.wojciechowska@hackney.gov.uk>

--- src/schemas/sole-to-joint/request-documents.ts
+++ src/schemas/sole-to-joint/request-documents.ts
@@ -5,11 +5,7 @@ import { appointmentSchema } from "../appointment";
 export const requestDocumentsSchema = (errorMessages: Record<string, string>) =>
   Yup.object({
     requestType: Yup.string().oneOf(["manual", "automatic"]).required(errorMessages.W4),
-    isDeclaration: Yup.boolean(),
-    declaration: Yup.boolean().when("isDeclaration", {
-      is: true,
-      then: Yup.boolean().oneOf([true], errorMessages.W4).required(errorMessages.W4),
-    }),
+    declaration: Yup.boolean().oneOf([true], errorMessages.W4).required(errorMessages.W4),
   }).concat(appointmentSchema(errorMessages));
 
 export type RequestDocumentsFormData = Yup.Asserts<
